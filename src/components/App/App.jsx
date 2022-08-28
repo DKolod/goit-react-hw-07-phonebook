e5193dc89a46593e18e439efed96479cba9ac498
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2022-08-28 19:58:02 +0300

    add react-toastify

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,52 +8,6 @@ import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
 
-// export function App() {
-//   const dispatch = useDispatch();
-//   const contacts = useSelector(state => state.contacts.items);
-//   const filter = useSelector(state => state.contacts.filter);
-
-//   const onSubmit = data => {
-//     const contactNames = contacts.map(contact => contact.name);
-
-//     if (contactNames.includes(data.name)) {
-//       alert(`${data.name} already have.`);
-//     } else {
-//       dispatch(addContact(data));
-//     }
-//   };
-
-//   const deleteContacts = contactId => {
-//     dispatch(deleteContact(contactId));
-//   };
-
-//   const changeFilter = evt => {
-//     dispatch(setContact(evt.target.value));
-//   };
-
-//   const filterContact = () => {
-//     const normalizedFilter = filter.toLowerCase();
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   const filteredContacts = filterContact();
-
-//   return (
-//     <div className={css.container}>
-//       <h1>Phonebook</h1>
-//       <ContactForm onSubmit={onSubmit} />
-//       <h2>Contacts</h2>
-
-//       <Filter value={filter} onChange={changeFilter} />
-//       <ContactList
-//         contacts={filteredContacts}
-//         onDeleteContact={deleteContacts}
-//       />
-//     </div>
-//   );
-// }
 export function App() {
   const [filter, setFilter] = useState('');
   const { data } = useGetContactsQuery();
@@ -83,13 +37,6 @@ export function App() {
 
       <Filter filter={filter} onChange={changeFilter} />
       <ContactList contacts={isVisibleContacts()} />
-
-      {/* <Filter value={filter} onChange={changeFilter} /> */}
-      {/* {filteredContacts.length
-            ? <ContactList
-              contacts={filteredContacts}
-              onDeleteContact={deleteContacts} />
-            : null} */}
     </div>
   );
 }
--- src/components/ContactList/ContactItem.jsx
+++ src/components/ContactList/ContactItem.jsx
@@ -4,10 +4,14 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const ContactItem = ({ id, name, phone }) => {
-  const [deleteContact, { isLoading: isDeliting }] = useDeleteContactMutation();
-  if (isDeliting) {
-    toast.success(`Deleted`);
-  }
+  const [deleteContact] = useDeleteContactMutation();
+  const handleDeleteContact = async id => {
+    await deleteContact(id);
+    toast.error('Deleted', {
+      duration: 2000,
+    });
+  };
+
   return (
     <li key={id} className={css.item}>
       <p>
@@ -16,9 +20,10 @@ const ContactItem = ({ id, name, phone }) => {
       <button
         className={css.btnDelete}
         type="button"
-        onClick={() => deleteContact(id)}
-        disabled={isDeliting}
-      ></button>
+        onClick={() => handleDeleteContact(id)}
+      >
+        Delete
+      </button>
     </li>
   );
 };
